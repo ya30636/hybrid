5ec4cacc609aedddabf1254e90865e62a00e3107
r04942103 <r04942103@linux2.csie.ntu.edu.tw>
2017-03-17 22:36:42 +0800

    add plot

--- hb.py
+++ hb.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import itertools
+import timeit
 #from numpy.linalg import norm
 #import scipy.linalg
 
@@ -21,14 +21,13 @@ T = np.transpose
 H = hermitian
 
 def mutual_coherence(A):
-	A_n = np.linalg.norm(A, axis=0, ord=2)
-	A /= A_n
-	return ( np.absolute( np.dot ( H(A), A ) ) - np.eye(A.shape[1]) ).max()
-	#return max( [ np.absolute( np.dot( A[:,i], A[:,j]) ) for i, j in itertools.product(range( A.shape[1] ), range(A.shape[1])) if i != j ] )
+    A_n = np.linalg.norm(A, axis=0, ord=2)
+    A /= A_n
+    return ( np.absolute( np.dot ( H(A), A ) ) - np.eye(A.shape[1]) ).max()
 
 def precoder(Nt, Lt):
-	sets = [1, -1, 1j, -1j]
-	return 1/np.sqrt(Nt) * np.random.choice( sets, (Nt, Lt) )
+    sets = [1, -1, 1j, -1j]
+    return 1/np.sqrt(Nt) * np.random.choice( sets, (Nt, Lt) )
 
 N = 50
 Nt = 128
@@ -36,44 +35,48 @@ Lt = 50
 M = 128
 L = 50
 
-X = np.eye(Lt, N)
-P = precoder(Nt, Lt)
-A_T = 1 / np.sqrt(Nt) * DFT_matrix(Nt)
+Times = 10
+mu_A = []
+mu_B = []
+mu_C = []
+for t in range(Times):
 
-sets = [1, -1, 1j, -1j]
-#P = np.random.choice( sets, (Nt, Lt) )
-#print T(X).shape 
-C = np.dot( np.dot ( T(X), T(P) ) , A_T )
-#np.kron()
-print mutual_coherence(C)
+    start = timeit.default_timer()
 
-F = DFT_matrix(M)
-n = np.random.choice(M, N)
-F_L = F[:, 0:L]
-B = F_L[n, :]
-print mutual_coherence(B)
+    X = np.eye(Lt, N)
+    P = precoder(Nt, Lt)
+    A_T = 1 / np.sqrt(Nt) * DFT_matrix(Nt)
+    C = np.dot( np.dot ( T(X), T(P) ) , A_T )
+    mu_C.append( mutual_coherence(C) )
+    print ( mutual_coherence(C) )
 
-print B.shape
-print C.shape
+    F = DFT_matrix(M)
+    n = np.random.choice(M, N)
+    F_L = F[:, 0:L]
+    B = F_L[n, :]
+    mu_B.append( mutual_coherence(B) )
+    print ( mutual_coherence(B) )
 
-A = np.zeros((N, L * Nt) , dtype=complex )
-for i in range(N):
-	A[i, :] = np.kron(B[i, :], C[i, :]) 
+    A = np.zeros((N, L * Nt) , dtype=complex )
+    for i in range(N):
+        A[i, :] = np.kron(B[i, :], C[i, :]) 
 
-# print A[0,1:3]
-# print A[1,1:3]
-print A.shape
-print mutual_coherence(A)
+    mu_A.append( mutual_coherence(A) )
+    print ( mutual_coherence(A) )
+
+    stop = timeit.default_timer()
+    print ( stop - start  )
 
 
 # plt.subplot(2, 1, 1)
-# plt.plot(x, y_sin)
-# plt.plot(x, y_cos)
-# plt.xlabel('x axis label')
-# plt.ylabel('y axis label')
-# plt.title('Sine and Cosine')
-# plt.legend(['Sine', 'Cosine'])
-# plt.grid()
+plt.plot( range(Times), mu_A)
+plt.plot( range(Times), mu_B)
+plt.plot( range(Times), mu_C)
+plt.xlabel('times')
+plt.ylabel('mutual coherence')
+plt.title('Analysis')
+plt.legend(['joint', 'ofdm', 'precoder'])
+plt.grid()
 # plt.subplot(2, 1, 2)
 # plt.plot(x, y_sin)
 # plt.plot(x, y_cos)
